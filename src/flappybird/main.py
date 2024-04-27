7ccc08fc7a4cbb97bbd749501138a0915d0d7627
Orenji <160479665+Orenji2@users.noreply.github.com>
2024-04-27 15:09:19 +0800

    Added Score Counter
    Scores are now shown in the top part of the screen

--- src/flappybird/main.py
+++ src/flappybird/main.py
@@ -15,6 +15,12 @@ screen_height = 936
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Flappy Bird')
 
+#FONT
+font = pygame.font.Font("fonts/score.ttf", 60)
+
+#COLOR 
+white = (255,255,255)
+
 #GAME_VARIABLES
 ground_scroll = 0
 scroll_speed = 4
@@ -23,10 +29,17 @@ game_over = False
 pipe_gap = 150
 pipe_frequency = 1500 #ms
 last_pipe = pygame.time.get_ticks() - pipe_frequency
+score = 0
+pass_pipe = False
 
 #LOAD_IMAGES
 bg = pygame.image.load('img/bg.png')
 ground_img = pygame.image.load('img/ground.png')
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
 
 #BIRD_CLASS
 class Bird(pygame.sprite.Sprite):
@@ -110,7 +123,7 @@ bird_group.add(flappy)
 
 run = True #sets the games if running
 
-#loop while game is running
+#MAIN GAME LOOP
 while run:
 
     clock.tick(fps) #controls the fps to 60
@@ -122,8 +135,19 @@ while run:
     bird_group.update()
     pipe_group.draw(screen)
 
-    #scroll the ground
+    #GROUND
     screen.blit(ground_img, (ground_scroll, 768))
+
+    #SCORE
+    if len(pipe_group) > 0:
+        if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.left and bird_group.sprites()[0].rect.right < pipe_group.sprites()[0].rect.right and pass_pipe==False:
+            pass_pipe = True
+        if pass_pipe == True:
+            if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.right:
+                score += 1
+                pass_pipe = False
+
+    draw_text(str(score), font, white, int(screen_width /2), 20)
 
     # PIPE COLLISION
     if pygame.sprite.groupcollide(bird_group, pipe_group, False, False) or flappy.rect.top < 0:
